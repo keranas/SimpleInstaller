6aa956fba3783af76140bcd32c4091915a11eb1d
keranas <51473430+keranas@users.noreply.github.com>
2019-06-07 10:14:14 +0300

    Add files via upload

---
+++ ApplInstaller/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace ApplInstaller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Install = new System.Windows.Forms.Button();
+            this.UnInstall = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.Program1 = new System.Windows.Forms.TextBox();
+            this.Version1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Install
+            // 
+            this.Install.BackColor = System.Drawing.SystemColors.Highlight;
+            this.Install.Location = new System.Drawing.Point(560, 236);
+            this.Install.Name = "Install";
+            this.Install.Size = new System.Drawing.Size(191, 48);
+            this.Install.TabIndex = 0;
+            this.Install.Text = "Install";
+            this.Install.UseVisualStyleBackColor = false;
+            this.Install.Click += new System.EventHandler(this.Install_Click);
+            // 
+            // UnInstall
+            // 
+            this.UnInstall.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.UnInstall.Location = new System.Drawing.Point(560, 308);
+            this.UnInstall.Name = "UnInstall";
+            this.UnInstall.Size = new System.Drawing.Size(191, 47);
+            this.UnInstall.TabIndex = 1;
+            this.UnInstall.Text = "UnInstall";
+            this.UnInstall.UseVisualStyleBackColor = false;
+            this.UnInstall.Click += new System.EventHandler(this.UnInstall_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(23, 64);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(655, 22);
+            this.textBox.TabIndex = 2;
+            this.textBox.Text = "c:\\Program Files\\akeramid\\MultiSpecNMR";
+            // 
+            // Program1
+            // 
+            this.Program1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.Program1.Location = new System.Drawing.Point(28, 28);
+            this.Program1.Name = "Program1";
+            this.Program1.ReadOnly = true;
+            this.Program1.Size = new System.Drawing.Size(361, 22);
+            this.Program1.TabIndex = 3;
+            // 
+            // Version1
+            // 
+            this.Version1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.Version1.Location = new System.Drawing.Point(424, 29);
+            this.Version1.Name = "Version1";
+            this.Version1.ReadOnly = true;
+            this.Version1.Size = new System.Drawing.Size(152, 22);
+            this.Version1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Tan;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(830, 477);
+            this.Controls.Add(this.Version1);
+            this.Controls.Add(this.Program1);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.UnInstall);
+            this.Controls.Add(this.Install);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Installer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Install;
+        private System.Windows.Forms.Button UnInstall;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.TextBox Program1;
+        private System.Windows.Forms.TextBox Version1;
+    }
+}
+
---
+++ ApplInstaller/Form1.cs
@@ -0,0 +1,343 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using IWshRuntimeLibrary;
+using System.IO.Compression;
+
+namespace ApplInstaller
+{
+    public partial class Form1 : Form
+    {
+        String path, path1, fileName, icon, texttext, license1, owner;
+
+        public Form1()
+        {
+            InitializeComponent();
+            if (System.IO.File.Exists(@"Install.ini") == true) //read Title and store in Title[500]
+            {
+                //Read the file line by line.  
+                System.IO.StreamReader file1 = new System.IO.StreamReader(@"Install.ini");
+
+                owner = file1.ReadLine();
+                texttext = file1.ReadLine();
+                Program1.Text = texttext;
+                Version1.Text = file1.ReadLine();
+                license1= file1.ReadLine();
+                textBox.Text = "C:/Program Files/" + owner + "/" + texttext + "/";
+                file1.Close();
+            }
+            else
+            {
+                MessageBox.Show("Install.ini is missing");
+                Application.Exit();
+            }
+
+
+        }
+
+        private void Install_Click(object sender, EventArgs e)
+        {
+
+
+            path1 = @textBox.Text;
+            path = @"";
+            texttext = Program1.Text;
+            fileName = texttext + ".exe";
+            icon = @"icon.ico";
+            Lsave();
+            movefile(path, path1, fileName, icon, texttext,license1);
+            moveallfiles(path, path1);
+            MessageBox.Show("The Program Installed Succesfully");
+            Application.Exit();
+        }
+        private void Lsave()
+        {
+            String Temp;
+            Temp=textBox.Text;
+            String ddd = @Program1.Text;
+            String LAname = "                                                                                                                         ";
+            LAname = @"C:/" + ddd + "/";
+            System.IO.Directory.CreateDirectory(LAname);
+            LAname = LAname + "L.txt";
+            System.IO.BinaryWriter LIni = new System.IO.BinaryWriter(System.IO.File.Open(LAname, System.IO.FileMode.Create));
+            LIni.Write(UTF8Encoding.Default.GetBytes(Temp));
+            LIni.Write('\r'); LIni.Write('\n');
+            DateTime thisDay = DateTime.Today;
+            LIni.Close();
+
+        }
+        private void Lread()
+        {
+            String Temp;
+            String W = @Program1.Text;
+            if (System.IO.File.Exists(@"C:\\"+W+@"\\L.txt") == true) //read Title and store in Title[500]
+            {
+                //Read the file line by line.  
+                System.IO.StreamReader file2 = new System.IO.StreamReader(@"C:\\" + W + "\\L.txt");
+
+
+                Temp = file2.ReadLine();
+                textBox.Text = Temp;
+                file2.Close();
+            }
+        }
+        private void UnInstall_Click(object sender, EventArgs e)
+        {
+            Lread();
+
+            System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(@textBox.Text);
+
+            foreach (System.IO.FileInfo file in di.GetFiles())
+            {
+                file.Delete();
+            }
+            foreach (System.IO.DirectoryInfo dir in di.GetDirectories())
+            {
+                dir.Delete(true);
+            }
+
+
+
+
+            String LAname;
+            LAname = textBox.Text + "\\" + Program1.Text + ".exe";
+            if (System.IO.File.Exists(LAname))
+            {
+                System.IO.File.Delete(LAname);
+            }
+            LAname = textBox.Text + "\\" + "icon.ico";
+            if (System.IO.File.Exists(LAname))
+            {
+                System.IO.File.Delete(LAname);
+            }
+            
+            LAname = textBox.Text;
+            if (System.IO.Directory.Exists(LAname))
+            {
+                System.IO.Directory.Delete(LAname);
+            }
+            LAname = @"C:/" + Program1.Text + "/L.txt";
+            if (System.IO.File.Exists(LAname))
+            {
+                System.IO.File.Delete(LAname);
+            }
+            LAname = @"C:/" + Program1.Text;
+            if (System.IO.Directory.Exists(LAname))
+            {
+               // System.IO.Directory.Delete(LAname);
+            }
+            //string WinUser = WindowsIdentity.GetCurrent().Name;
+            //WinUser = WinUser.Substring(WinUser.LastIndexOf("\\") + 1);
+
+            string pathD = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            if (System.IO.File.Exists(pathD +"\\"+ texttext + ".lnk"))
+                System.IO.File.Delete(pathD + "\\" + texttext + ".lnk");
+            string commonStartMenuPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonStartMenu);
+            string appStartMenuPath = System.IO.Path.Combine(commonStartMenuPath, "Programs", texttext);
+            if (System.IO.File.Exists(appStartMenuPath + "\\"+texttext + ".lnk"))
+                System.IO.File.Delete(appStartMenuPath + "\\" + texttext + ".lnk");
+            if (System.IO.File.Exists(appStartMenuPath + "\\" + "Uninstall.lnk"))
+                System.IO.File.Delete(appStartMenuPath + "\\" + "Uninstall.lnk");
+            if (System.IO.Directory.Exists(appStartMenuPath))
+                System.IO.Directory.Delete(appStartMenuPath);
+
+
+            MessageBox.Show("The Program UnInstalled Succesfully");
+            Application.Exit();
+        }
+        private  void movefile(string path, string path2, string fileName, string icon, string texttext, string license1)
+        {
+
+            if (!System.IO.Directory.Exists(path2))
+            {
+                System.IO.Directory.CreateDirectory(@path2);
+            }
+
+                try
+                {
+                String ss = path2 + " \\ "+ fileName;
+    
+                String so = @path ;
+                string sourceFile = System.IO.Path.Combine(path, texttext);
+                string sourceIcon= System.IO.Path.Combine(path, icon);
+                string destFile = System.IO.Path.Combine(path2, fileName);
+                string destIcon = System.IO.Path.Combine(path2, icon);
+                //string ssourceFile = System.IO.Path.Combine(path, license1);
+                //string ddestFile = System.IO.Path.Combine(path2, license1);
+                if (System.IO.File.Exists(destFile))
+                    System.IO.File.Delete(destFile);
+                if (System.IO.File.Exists(destIcon))
+                    System.IO.File.Delete(destIcon);
+                //if (System.IO.File.Exists(ddestFile))
+                   // System.IO.File.Delete(ddestFile);
+                string pathD = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                if (System.IO.File.Exists(pathD + "\\" + texttext + ".lnk"))
+                    System.IO.File.Delete(pathD + "\\" + texttext + ".lnk");
+                string commonStartMenuPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonStartMenu);
+                string appStartMenuPath = System.IO.Path.Combine(commonStartMenuPath, "Programs", texttext);
+                if (System.IO.File.Exists(appStartMenuPath + "\\" + texttext + ".lnk"))
+                    System.IO.File.Delete(appStartMenuPath + "\\" + texttext + ".lnk");
+                // Move the file
+                if (System.IO.File.Exists(sourceFile))
+                {
+                    System.IO.File.Copy(sourceFile, destFile);
+                }
+                else
+                {
+                    MessageBox.Show("There is no source file");
+                    Application.Exit();
+                }
+
+               // if (System.IO.File.Exists(ssourceFile))
+               // {
+                ////    System.IO.File.Copy(ssourceFile, ddestFile);
+               // }
+               // else
+               // {
+               //     System.IO.File.Create(@ddestFile);
+               // }
+
+
+                if (System.IO.File.Exists(sourceIcon))
+                {
+                    System.IO.File.Copy(sourceFile, destIcon);
+                }
+                else
+                {
+                    MessageBox.Show("There is no source file");
+                    Application.Exit();
+                }
+
+                //commonStartMenuPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonStartMenu);
+                //appStartMenuPath = System.IO.Path.Combine(commonStartMenuPath, "Programs", texttext);
+
+                String W = Program1.Text;
+                String tempDir = "C:\\" + W;
+                String sourceFileInst = "Install.exe";
+                String txtemp = "Install.ini";
+                String txtempico = "Install.ico";
+                String destFileInst = tempDir + "/Uninstall.exe";
+                String txtempDest = tempDir + "/Install.ini";
+                String txtempicoDest = tempDir + "/Install.ico";
+                if (System.IO.Directory.Exists(tempDir) == true) //read Title and store in Title[500]
+                {
+                    if (System.IO.File.Exists(destFileInst))
+                    {
+                        System.IO.File.Delete(destFileInst);
+                    }
+                    if (System.IO.File.Exists(txtempDest))
+                    {
+                        System.IO.File.Delete(txtempDest);
+                    }
+                    if (System.IO.File.Exists(txtempicoDest))
+                    {
+                        System.IO.File.Delete(txtempicoDest);
+                    }
+                    System.IO.File.Copy(@sourceFileInst, @destFileInst);
+                    System.IO.File.Copy(@txtemp, @txtempDest);
+                    System.IO.File.Copy(@txtempico, @txtempicoDest);
+                }
+
+                string shortcutLocation = System.IO.Path.Combine(@pathD, @texttext + @".lnk");
+                WshShell shell = new WshShell();
+                IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutLocation);
+                shortcut.Description = texttext;   // The description of the shortcut
+                shortcut.IconLocation = @destIcon;           // The icon of the shortcut
+                shortcut.TargetPath = @destFile;                 // The path of the file that will launch when the shortcut is run
+                shortcut.WorkingDirectory = @path2;
+                shortcut.Save();
+
+
+
+                if (!System.IO.Directory.Exists(appStartMenuPath))
+                {
+                    System.IO.Directory.CreateDirectory(appStartMenuPath);
+                }
+                shortcutLocation = appStartMenuPath + "\\" + texttext + ".lnk";
+                WshShell shell1 = new WshShell();
+                IWshShortcut shortcut1 = (IWshShortcut)shell1.CreateShortcut(shortcutLocation);
+                shortcut1.Description = texttext;   // The description of the shortcut
+                shortcut1.IconLocation = @destIcon;           // The icon of the shortcut
+                shortcut1.TargetPath = @destFile;                 // The path of the file that will launch when the shortcut is run
+                shortcut1.WorkingDirectory = @path2;
+                shortcut1.Save();
+
+
+               String  destIconInst = "Install.ico";
+               
+                shortcutLocation = appStartMenuPath + "\\" + "Uninstall" + ".lnk";
+                WshShell shell2 = new WshShell();
+                IWshShortcut shortcut2 = (IWshShortcut)shell2.CreateShortcut(shortcutLocation);
+                shortcut2.Description = "Uninstall";   // The description of the shortcut
+                shortcut2.IconLocation = @txtempicoDest;           // The icon of the shortcut
+                shortcut2.TargetPath = @destFileInst;                 // The path of the file that will launch when the shortcut is run
+                shortcut2.WorkingDirectory = @tempDir;
+                shortcut2.Save();
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("The process failed");
+            }
+        }
+        //Now Create all of the directories
+        private  void moveallfiles(string SourcePath, string DestinationPath)
+        {
+            SourcePath=System.IO.Directory.GetCurrentDirectory();
+            foreach (string dirPath in System.IO.Directory.GetDirectories(SourcePath, "*",
+                System.IO.SearchOption.AllDirectories))
+                System.IO.Directory.CreateDirectory(dirPath.Replace(SourcePath, DestinationPath));
+
+            //Copy all the files & Replaces any files with the same name
+            foreach (string newPath in System.IO.Directory.GetFiles(SourcePath, "*.*",
+                System.IO.SearchOption.AllDirectories))
+            {
+                
+                //check the extention
+                string ext = System.IO.Path.GetExtension(newPath);
+                if(ext==".zip")
+                        {
+                    unziptemp(newPath, DestinationPath);
+                }
+                else
+                {
+                    System.IO.File.Copy(newPath, newPath.Replace(SourcePath, @DestinationPath), true);
+                }
+            }
+            // Delete the main program without the extention
+            string destFile = System.IO.Path.Combine(DestinationPath, Program1.Text);
+            System.IO.File.Delete(destFile);
+        }
+        private void unziptemp(string newPath, string DestinationPath)
+        {
+            String LAname;
+            LAname = @"C:\\1234tzip";
+            System.IO.Directory.CreateDirectory(LAname);
+            //string startPath = @"c:\example\start";
+            // string zipPath = @"c:\example\result.zip";
+            //string extractPath = @"c:\example\extract";
+
+            //System.IO.Compression.ZipFile.CreateFromDirectory(startPath, zipPath);
+            System.IO.Compression.ZipFile.ExtractToDirectory(newPath, LAname);
+            foreach (string dirPath in System.IO.Directory.GetDirectories(LAname, "*",
+                System.IO.SearchOption.AllDirectories))
+                System.IO.Directory.CreateDirectory(dirPath.Replace(LAname, DestinationPath));
+            foreach (string nPath in System.IO.Directory.GetFiles(LAname, "*.*",
+                System.IO.SearchOption.AllDirectories))
+            {
+                    System.IO.File.Copy(nPath, nPath.Replace(LAname, @DestinationPath), true);
+            }
+
+            System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(LAname);
+            foreach (System.IO.FileInfo file in di.GetFiles())
+            {
+                file.Delete();
+            }
+            foreach (System.IO.DirectoryInfo dir in di.GetDirectories())
+            {
+                dir.Delete(true);
+            }
+            System.IO.Directory.Delete(LAname);
+        }
+    }
+}
